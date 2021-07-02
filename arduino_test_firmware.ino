71cdc8d7bf8b869158177c88772dcc3e6a307c09
Miguel-Carreon <a01566368@itesm.mx>
2021-07-01 21:56:55 -0600

    same as previous version but commented

--- arduino_test_firmware.ino
+++ arduino_test_firmware.ino
@@ -1,9 +1,41 @@
+/*===========================================================================================================
+      arduino_test_firmware
+
+      This code is for testing and developing different functions for a 16 keys (4x4 matrix) keyboard.
+      The intention of this is to have a macro keyboard powered by a Pro Micro development board and
+      programed using Arduino, to make key combinations and other macro type features with the press
+      of a single button.
+      
+      As for the components, currently a 4x4 generic matrix keyboard is being used, it only has 8 pins,
+      4 being used as inputs and 4 as outputs. But any matrix configuration will work if the Matrix
+      keyboard arrangement section is coded properly, regardless of the diode protection in each key,
+      one thing to consider when coding the matrix is the diode orientation.
+
+      In this case a generic pro micro is the board of choice, hence the GPIO used are the pins from 2 to 9,
+      since the Arduino Keypad library is used for this sketch, pins from 2-5 are used as inputs and 6-9 are
+      used as output, this is because this library lets current pass from row to column, and those are the
+      pinouts configured in this sketch to be used as rows and columns, respectively. If you are using a
+      simple matrix keyboard that has normally open switches you do not need to worry about the current
+      direction, if you wire the keyboard differently just be sure to adjust the code accordingly. In case
+      you have a diode protected matrix keyboard you will need to pay attention to the rows and columns
+      configuration, although this protection is not needed since this library sets the unused pins to a
+      high impedance, therefore protecting itself, adding diodes to your switches is just a safety
+      redundancy.
+
+      Created 28 Jun 2021
+      By; Miguel Carreón
+      Modiffied 1 Jul 2021
+      By;  Miguel Carreón
+
+      https://github.com/Miguel-Carreon/arduinoKeeb_firmware_test
+===========================================================================================================*/
 #include <Keypad.h>
 #include <Keyboard.h>
 
-char holdKey;
-unsigned long holdTime;
-int level = 0;
+//  Global variables declaration  //
+char holdKey; //Sores a key that is being holded
+unsigned long holdTime; //Stores time to compare it to current time in order to know if a key is being holded
+int level = 0; //Indicates the current keyboard level
 
 const byte ROWS = 4; //The keeboard has four rows
 const byte COLS = 4; //The keeboard has four columns
@@ -19,23 +51,25 @@ char matrixKeeb[ROWS][COLS] = {
 byte rowPins[ROWS] = {9, 8, 7, 6}; //Respective pinout for the rows
 byte colPins[COLS] = {5, 4, 3, 2}; //Respective pinout for the columns
 
-//Initializa a new Keypad class
+// Initialize a new Keypad class //
 Keypad keypad = Keypad(makeKeymap(matrixKeeb), rowPins, colPins, ROWS, COLS);
 
 void setup() {
   
-  Serial.begin(9600);
-  Keyboard.begin(); //Starts HID device
+  Serial.begin(9600); //Baud rate to 9600
+  Keyboard.begin(); //Starts keyboard HID device
 
 }
 
+// Funtion to ignore mechanical bouncing  //
 void debounce(int d){
       
-  delay(d);
+  delay(d); //The argument in the debounce funtion declares the duration of the delay
   Keyboard.releaseAll(); //Releases keys
     
 }
 
+// Function to pres CTRL + SHIFT + ANYKEY //
 void csMacro(uint8_t key){
   
   Keyboard.press(KEY_LEFT_CTRL);
@@ -44,6 +78,7 @@ void csMacro(uint8_t key){
   
 }
 
+// Funtion to press CTRL + ANYKEY //
 void cMacro(uint8_t key){
   
   Keyboard.press(KEY_LEFT_CTRL);
@@ -51,20 +86,22 @@ void cMacro(uint8_t key){
   
 }
 
+// Funtion to press CTRL + ANYKEY //
 void sKey(uint8_t key){
   Keyboard.press(KEY_LEFT_SHIFT);
   Keyboard.press(key);
 }
 
-
+// Funtion to use windows run (WINDOWS_KEY + R) //
 void windowsRun(int d, String command){
 
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('r');
   Keyboard.release(KEY_LEFT_GUI);
   Keyboard.release('r');
-  delay(d);
-  Keyboard.print(command);
+  delay(d); //This delay is controlled by the first statement of this function argument
+  //The ammount of tyme may vary on the system, the faster your computer the less time you should be able put
+  Keyboard.print(command); //The seccond statement of this function's argument is what is printed, it should be a string inside double quotes e.g. "arduino.cc"
   Keyboard.releaseAll();
   delay(10);
   Keyboard.press(KEY_RETURN);
@@ -72,28 +109,36 @@ void windowsRun(int d, String command){
   
 }
 
+// Function to write Discord emojis //
 void discordEmoji(String Emoji){
-  Keyboard.print('>');
-  Keyboard.print(Emoji);
-  Keyboard.print('>');
+  Keyboard.print('>'); //The > character is what makes a colon to be printed in an Spanish (México) ANSI keyboard.
+  Keyboard.print(Emoji); //The name of the emoji shall be written inside double quotes in the argument of this function e.g. "smile"
+  Keyboard.print('>'); // If your keyboard language is English change both > for :
   debounce(100);
 }
 
 void loop() {
 
-  char key = keypad.getKey();
-
+  char key = keypad.getKey(); //Stores the key pressed
+  
+  // Key single press //
   if (key){
-    holdKey = key;
+    holdKey = key; //Stores the key pressed for the hold key section
     Serial.println(key);
-    Keyboard.releaseAll();
+    Keyboard.releaseAll(); //Each iteration release all keys
+    
+    /*Layer system, each time * is pressed in the matrix keyboard a new layer of keys is selected,
+    this is usefull to virtually have more keys with the same ammount of physical keys.
+    Each time * is pressed you go to the next layer, you start at layer 0, when you reach level 2 
+    you start over at 0.*/
     if (key == '*'){
       level++;
-      if (level > 2){
+      if (level > 2){ //Change this number to the ammount of layers desired
         level = 0;
       }
     } 
 
+    //Level zero
     if (level == 0){
       switch(key){
         case '1':
@@ -121,6 +166,7 @@ void loop() {
         }
     }
 
+    //Level one
     else if (level == 1){
       switch(key){
         case '1':
@@ -145,6 +191,8 @@ void loop() {
           break;
       }
     }
+    
+    //Level two
     else if (level == 2){
       switch(key){
         case '1':
@@ -193,9 +241,12 @@ void loop() {
     }
   }
 
+  // Key hold //
   if (keypad.getState() == HOLD){
     if ((millis() - holdTime) > 100){
       //Available space for key commands
+
+      //Level two
       if (level == 2){
         switch (holdKey){
           case '1':
@@ -207,3 +258,6 @@ void loop() {
     }
   }
 }
+
+/* Levels for the sigle key press and key hold are the same, just put the command in the corresponding
+ *  desired function*/
